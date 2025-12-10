0331138f0e679629a95cc50b56d70528211d3a79
RP-Experience <github@hartlmax.de>
2025-12-10 16:02:40 +0100

    Enhance config with dispatch settings for alerts
    Added configuration for dispatch settings including police and medical alerts.

--- config.lua
+++ config.lua
@@ -1,10 +1,6 @@
 Config = {}
 Config.Debug = false
 
-QBCore = exports['qb-core']:GetCoreObject()  -- uncomment if you use QBCore
--- ESX = exports["es_extended"]:getSharedObject() -- uncomment if you use ESX
-
-
 Config.Settings = {
 	Framework = 'QB', -- QB/ESX
 	Target = "OX", -- OX/QB
@@ -13,5 +9,27 @@ Config.Settings = {
 	ItemName = "c4_bomb",
 	Beeps = true, -- if set to true the bomb will play beeping sound
 	PoliceAlert = true,
-	Dispatch = 'ps', -- ps-dispatch supported only. if you want to use custom set it to custom and edit function AlertPolice().
+	Dispatch = 'ps', -- ps / md / both / auto / custom (custom = edit AlertPolice() in client/main.lua)
+}
+
+Config.Dispatch = {
+    -- Used when Config.Settings.Dispatch = 'md' / 'both' / 'auto'
+    md = {
+        jobs    = { 'police' }, -- ESX job names that should receive the alert
+        blip    = 161,          -- GTA blip sprite id
+        title   = 'Bomb Threat',
+        message = 'Possible bomb reported.',
+    },
+
+    -- Used when Config.Settings.Dispatch = 'ps' / 'both' / 'auto'
+    ps = {
+        message      = 'Bomb Threat',
+        dispatchCode = 'ICD-10 Bomb Threat',
+        description  = 'Bomb Threat',
+        radius       = 5,
+        sprite       = 653,
+        color        = 21,
+        scale        = 1.0,
+        length       = 100,
+    }
 }
